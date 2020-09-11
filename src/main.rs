e039ab88a11385d54e1b09ce864af1f5b550fa51
Chris <cwt271@nyu.edu>
2020-09-11 10:24:01 -0500

    Initializes tracer list with random positions in domain

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 use ndarray::{Array, Axis, Ix2};
 use ndarray_ops::*;
-mod tracers
+mod tracers;
 
 static TAU: f64 = 2.0 * std::f64::consts::PI;
 
@@ -14,7 +14,7 @@ pub struct Grid
     pub cell_centers   : Array<(f64, f64), Ix2>,
     pub face_centers_x : Array<(f64, f64), Ix2>,
     pub face_centers_y : Array<(f64, f64), Ix2>,
-    pub tracers        : Vec<Tracer>,
+    pub tracer_list    : Vec<tracers::Tracer>,
 }
 
 pub struct Velocities
@@ -25,7 +25,7 @@ pub struct Velocities
 
 impl Velocities
 {
-    fn initialize_sin(grid: Grid) -> Velocities
+    fn initialize_sine(grid: Grid) -> Velocities
     {
         return Velocities{
             face_vx: grid.face_centers_x.mapv(|xy: (f64, f64)| -> f64 {let (x, _) = xy; x.sin()}),
@@ -63,19 +63,26 @@ pub fn face_centers_y(domain_radius: f64, block_size: usize) -> Array<(f64, f64)
     return cartesian_product2(xc, yv);
 }
 
+pub fn init_tracer_list(domain_radius: f64, ntracers: usize) -> Vec<tracers::Tracer>
+{
+    let tracers: Vec<_> = (0..ntracers).map(|_| tracers::Tracer::randomize(domain_radius)).collect();
+    return tracers;
+}
+
 
 
 
 // ============================================================================
-fn run(domain_radius: f64, block_size: usize) -> () 
+fn run(domain_radius: f64, block_size: usize, ntracers: usize) -> () 
 {
     let grid = Grid{
         cell_centers  : cell_centers  (domain_radius, block_size),
         face_centers_x: face_centers_x(domain_radius, block_size),
         face_centers_y: face_centers_y(domain_radius, block_size),
+        tracer_list   : init_tracer_list(domain_radius, ntracers),
     };
 
-    let _vfield = Velocities::initialize_sin(grid);
+    let _vfield = Velocities::initialize_sine(grid);
 }
 
 
@@ -86,7 +93,8 @@ fn main()
 {
     println!("Hello, world!");
 
-    let domain_radius     = TAU;
-    let block_size: usize = 64;
-    run(domain_radius, block_size);
+    let domain_radius      = TAU;
+    let block_size: usize  = 64;
+    let num_tracers: usize = 100;
+    run(domain_radius, block_size, num_tracers);
 }
--- src/tracers.rs
+++ src/tracers.rs
@@ -1,11 +1,14 @@
 // use ndarray::{Axis, Array, Ix2};
+use rand::Rng;
+
 
 
 
 // ============================================================================
 pub struct Tracer
 {
-    pub location: (f64, f64),
+    pub x: f64,
+    pub y: f64,
 }
 
 
@@ -14,9 +17,24 @@ pub struct Tracer
 // ============================================================================
 impl Tracer
 {
-    fn find_cell(&self, grid) -> index {}
+    pub fn default() -> Tracer
+    {
+        return Tracer{x: 0.0, y: 0.0};
+    }
 
-    fn compute_velocity(index) -> (vx, vy) {}
 
-    fn update((vx, vy), dt) -> Tracer {}
+    pub fn randomize(domain_radius: f64) -> Tracer
+    {
+        let mut rng = rand::thread_rng();
+        let rand_x = rng.gen_range(-domain_radius, domain_radius);
+        let rand_y = rng.gen_range(-domain_radius, domain_radius);
+        return Tracer{x: rand_x, y: rand_y};
+    }
+
+    // fn find_cell(&self, grid) -> index {}
+
+    // fn compute_velocity(index) -> (vx, vy) {}
+
+    // fn update((vx, vy), dt) -> Tracer {}
+
 }
